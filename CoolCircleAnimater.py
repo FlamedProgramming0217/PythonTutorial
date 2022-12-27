aad6b2d5b5ce3d8b7b3f7ece948a5e587b242530
Huan Chen <78980723+FlamedProgramming0217@users.noreply.github.com>
2022-12-27 15:36:47 -0600

    Update CoolCircleAnimater.py
    Deleted excess comments

--- CoolCircleAnimater.py
+++ CoolCircleAnimater.py
@@ -4,16 +4,12 @@ window = turtle.Screen()
 turtle.Screen().bgcolor('black')
 t = turtle.Turtle()
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-
-#Asks for the speed and denies the answer when the input is not a number
 speed = input("How fast we going? 0 is fastest.\n")
 while True:
     if speed.isdigit():
         speed = int(speed)
         break
     speed = input("Incorrect answer, how fast we going? 0 is fastest.\n")
-
-#Asks for how long the program should run. The number is for the "for i in range".
 time = input(
     "How long should the program run? 500 is recommended: about 18 seconds.\n")
 while True:
@@ -23,8 +19,6 @@ while True:
     speed = input(
         "Incorrect answer, how long should the program run? 500 is recommended; about 18 seconds.\n"
     )
-
-#Asks for the size of the circle. The variable is put into how forward the turtle should go in each round of the "for i in range"
 size = input(
     "How big should the animation circle be? 50 seems like a good number.\n")
 while True:
@@ -34,8 +28,6 @@ while True:
     size = input(
         "Incorrect answer, How big should the animation circle be? 50 seems like a good number.\n"
     )
-
-#Assigns speed and draws the animation
 t.speed(speed)
 for i in range(time):
     t.color(colors[i % 6])
