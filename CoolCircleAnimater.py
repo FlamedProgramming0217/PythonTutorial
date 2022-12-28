166e6adf48b3558616e9e0cecb0266bfa8201f18
Huan Chen <78980723+FlamedProgramming0217@users.noreply.github.com>
2022-12-28 12:17:22 -0600

    Added blank lines for better visual and added background color support

--- CoolCircleAnimater.py
+++ CoolCircleAnimater.py
@@ -1,34 +1,34 @@
-
 import turtle
+
 window = turtle.Screen()
-turtle.Screen().bgcolor('black')
+background = input("What should the background color be?")
+turtle.Screen().bgcolor(background)
+
 t = turtle.Turtle()
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
 speed = input("How fast we going? 0 is fastest.\n")
 while True:
     if speed.isdigit():
         speed = int(speed)
         break
     speed = input("Incorrect answer, how fast we going? 0 is fastest.\n")
-time = input(
-    "How long should the program run? 500 is recommended: about 18 seconds.\n")
+t.speed(speed)
+
+time = input("How long should the program run? 500 is recommended: about 18 seconds.\n")
 while True:
     if time.isdigit():
         time = int(time)
         break
-    speed = input(
-        "Incorrect answer, how long should the program run? 500 is recommended; about 18 seconds.\n"
-    )
-size = input(
-    "How big should the animation circle be? 50 seems like a good number.\n")
+    time = input("Incorrect answer, how long should the program run? 500 is recommended; about 18 seconds.\n")
+
+size = input("How big should the animation circle be? 50 seems like a good number.\n")
 while True:
     if size.isdigit():
         size = int(size)
         break
-    size = input(
-        "Incorrect answer, How big should the animation circle be? 50 seems like a good number.\n"
-    )
-t.speed(speed)
+    size = input("Incorrect answer, How big should the animation circle be? 50 seems like a good number.\n")
+
 for i in range(time):
     t.color(colors[i % 6])
     t.forward(size)
