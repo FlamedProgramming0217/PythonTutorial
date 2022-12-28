2e835a2eb93a0cea2799c8f4242a08b97ae6afd6
Huan Chen <78980723+FlamedProgramming0217@users.noreply.github.com>
2022-12-28 12:20:04 -0600

    Added blank lines for better visuals

--- Spiral.py
+++ Spiral.py
@@ -1,23 +1,30 @@
 import turtle
+
 window = turtle.Screen()
+
 background = ("What should the background color be?")
 turtle.Screen().bgcolor(background)
+
 t = turtle.Turtle()
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
 speed = input("How fast do you want the spiral to draw? 0 is fastest.\n")
 while True:
     if speed.isdigit():
         speed = int(speed)
         break
     speed = input("Incorrect answer, how fast do you want the spiral to draw? 0 is fastest.\n")
+t.speed(speed)
+
 time = input("How long should the program run? 500 is recommended, covers most of the canvas.\n")
 while True:
     if time.isdigit():
         time = int(time)
         break
     speed = input("Incorrect answer, how long should the spiral draw? 500 is recommended, covers most of the canvas\n")
+
 curve = input("How much curve should the program have? 58 - 60, with 60 having no curve at all and 58 having most curve.")
-t.speed(speed)
+
 for i in range(time):
     t.color(colors[i % 6])
     t.forward(i)
