bb8df66b62e7367ace0feef7c3f022a8ce4ee815
Huan Chen <78980723+FlamedProgramming0217@users.noreply.github.com>
2022-12-28 12:06:32 -0600

    Spiral.py has changes in the code

--- Spiral.py
+++ Spiral.py
@@ -3,25 +3,25 @@ window = turtle.Screen()
 turtle.Screen().bgcolor('black')
 t = turtle.Turtle()
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
-speed = input("How fast we going? 0 is fastest.\n")
+speed = input("How fast do you want the spiral to draw? 0 is fastest.\n")
 while True:
     if speed.isdigit():
         speed = int(speed)
         break
-    speed = input("Incorrect answer, how fast we going? 0 is fastest.\n")
+    speed = input("Incorrect answer, how fast do you want the spiral to draw? 0 is fastest.\n")
 time = input(
-    "How long should the program run? 500 is recommended: about 18 seconds.\n")
+    "How long should the program run? 500 is recommended, covers most of the canvas.\n")
 while True:
     if time.isdigit():
         time = int(time)
         break
     speed = input(
-        "Incorrect answer, how long should the spiral draw? 500 is recommended.\n"
+        "Incorrect answer, how long should the spiral draw? 500 is recommended, covers most of the canvas\n"
     )
 t.speed(speed)
 for i in range(time):
     t.color(colors[i % 6])
     t.forward(i)
-    t.left(59)
+    t.left(58)
 
 window.exitonclick()
